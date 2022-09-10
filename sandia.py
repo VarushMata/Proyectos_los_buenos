5ac8304373981d518cc89aeab6d3c05bb06ae4d6
VarushMata <vmatap1600@alumno.ipn.mx>
2022-09-09 22:38:36 -0500

    sencillo pero BIEEEEEEEN

--- sandia.py
+++ sandia.py
@@ -1,8 +1,9 @@
 #Programa que recibe como entrada el peso de una sandía y muestra si se puede partir en números pares
 kilos = int(input(''))
-if kilos % 2 == 0:
-    print('SI')
-else: 
+if kilos == 2 or kilos == 1 or kilos == 0:
     print('NO')
-
-        
+else:
+    if kilos % 2 == 0:
+        print('SI')
+    else:
+        print('NO') 
